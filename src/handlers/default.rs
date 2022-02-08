4d3a2562eafdcf9178cfc0f882f8c975f216746c
rstular <rok@stular.eu>
2022-02-08 02:10:46 +0100

    enhance(default): Store URLs for 404 responses

--- src/handlers/default.rs
+++ src/handlers/default.rs
@@ -1,10 +1,21 @@
+use crate::db::models::HandlerEvent;
 use crate::handler::HandlerResponse;
+use crate::handler::{get_header_value, get_ip_address};
 use actix_web::{web::Bytes, HttpRequest, HttpResponse};
 
-pub fn handler(_bytes: Bytes, _req: &HttpRequest) -> HandlerResponse {
+pub const HANDLER_NAME: &str = "default";
+
+pub fn handler(_bytes: Bytes, req: &HttpRequest) -> HandlerResponse {
     HandlerResponse {
         http_response: HttpResponse::NotFound().body("404 - Not Found"),
-        handler_event: None,
+        handler_event: Some(
+            HandlerEvent::new(HANDLER_NAME)
+                .set_host(get_header_value(req, "Host"))
+                .set_x_forwarded_for(get_header_value(req, "X-Forwarded-For"))
+                .set_src_ip(get_ip_address(req))
+                .set_user_agent(get_header_value(req, "User-Agent"))
+                .set_uri(req.uri().to_string()),
+        ),
         report: None,
     }
 }
