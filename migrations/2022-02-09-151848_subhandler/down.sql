6c40c46c450cf848ebdf037ae2a4e5b05b839db7
rstular <rok@stular.eu>
2022-02-09 16:35:29 +0100

    fix(db): Wrong table name used in migrations

--- migrations/2022-02-09-151848_subhandler/down.sql
+++ migrations/2022-02-09-151848_subhandler/down.sql
@@ -1,4 +1,4 @@
 -- This file should undo anything in `up.sql`
-ALTER TABLE devil DROP COLUMN subhandler;
-ALTER TABLE devil RENAME COLUMN handler_data TO details;
+ALTER TABLE handler_events DROP COLUMN subhandler;
+ALTER TABLE handler_events RENAME COLUMN handler_data TO details;
 DROP INDEX CONCURRENTLY idx_subhandler;
--- migrations/2022-02-09-151848_subhandler/up.sql
+++ migrations/2022-02-09-151848_subhandler/up.sql
@@ -1,4 +1,4 @@
 -- Your SQL goes here
-ALTER TABLE devil ADD COLUMN subhandler VARCHAR;
-ALTER TABLE devil RENAME COLUMN details TO handler_data;
-CREATE INDEX CONCURRENTLY idx_subhandler ON devil(subhandler);
+ALTER TABLE handler_events ADD COLUMN subhandler VARCHAR;
+ALTER TABLE handler_events RENAME COLUMN details TO handler_data;
+CREATE INDEX CONCURRENTLY idx_subhandler ON handler_events(subhandler);
