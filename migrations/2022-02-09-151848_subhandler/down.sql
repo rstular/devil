980608e3d4aeab932e2d92f54e8a1cb6912118c7
rstular <rok@stular.eu>
2022-02-09 16:38:33 +0100

    fix(db): Creating an index cannot be done concurrently
    This is due to diesel migrations running in transaction blocks.

--- migrations/2022-02-09-151848_subhandler/down.sql
+++ migrations/2022-02-09-151848_subhandler/down.sql
@@ -1,4 +1,4 @@
 -- This file should undo anything in `up.sql`
 ALTER TABLE handler_events DROP COLUMN subhandler;
 ALTER TABLE handler_events RENAME COLUMN handler_data TO details;
-DROP INDEX CONCURRENTLY idx_subhandler;
+DROP INDEX idx_subhandler;
--- migrations/2022-02-09-151848_subhandler/up.sql
+++ migrations/2022-02-09-151848_subhandler/up.sql
@@ -1,4 +1,4 @@
 -- Your SQL goes here
 ALTER TABLE handler_events ADD COLUMN subhandler VARCHAR;
 ALTER TABLE handler_events RENAME COLUMN details TO handler_data;
-CREATE INDEX CONCURRENTLY idx_subhandler ON handler_events(subhandler);
+CREATE INDEX idx_subhandler ON handler_events(subhandler);
